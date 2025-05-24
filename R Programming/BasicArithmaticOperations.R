48d259d889d75fe93ba259cc3ab8bfa93c4a87d0
THAMILMANIVARATHARAJAN <thamilmani.infoteamptf@gmail.com>
2025-05-24 15:56:51 +0530

    R Programing

---
+++ R Programming/BasicArithmaticOperations.R
@@ -0,0 +1,24 @@
+a=2
+b=5
+print(a+b)
+
+c=a+b
+print(c)
+print(class(c))
+
+print(b-a)
+print(b*a)
+print(b/a)
+
+a = c(1,2,3)
+b = c(4,5,6)
+print(class(print(a+b)))
+print(class(a))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)
+
+a=c('a',23,"Vidyalaya",4.4)
+print(a)

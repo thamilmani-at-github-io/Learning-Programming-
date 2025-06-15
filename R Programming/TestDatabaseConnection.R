a178b84137855b79e41dfde3d7cf866649e42dd0
THAMILMANI VARATHARAJAN <v.kishoremani@gmail.com>
2025-06-15 16:59:08 +0530

    `Android

---
+++ R Programming/TestDatabaseConnection.R
@@ -0,0 +1,30 @@
+#Install RMysql Package ...
+
+library(RMySQL)
+library(DBI)
+
+drv <- dbDriver("MySQL")
+mysqlconnection = dbConnect(drv,username='root',password='',dbname='test1',host='localhost')
+dbListTables(mysqlconnection)
+
+ItemMaster <- dbSendQuery(mysqlconnection,"select * from Items")
+data.frame <- fetch(ItemMaster)
+print(data.frame)
+
+Transaction <- dbSendQuery(mysqlconnection,"select * from Transaction")
+data.frame <- fetch(Transaction)
+print(data.frame)
+
+TransactionAmount <- dbSendQuery(mysqlconnection,"select T.UID,I.ItemCode,I.ItemName,T.ItemQty,I.ItemRate,
+                                (I.ItemRate * T.ItemQty)as NetAmount
+                                 from Transaction T inner join Items I on I.ItemCode = T.ItemCode ")
+data.frame <- fetch(TransactionAmount)
+print(data.frame)
+
+ItemAmount <- dbSendQuery(mysqlconnection,"select I.ItemCode,I.ItemName,
+                                sum((I.ItemRate * T.ItemQty))as NetAmount
+                                from Transaction T inner join Items I on I.ItemCode = T.ItemCode 
+                                group by I.ItemCode,I.ItemName
+                          ")
+data.frame <- fetch(ItemAmount)
+print(data.frame)

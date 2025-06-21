091cf5daa45830b6f78f0b9c2f2697d326ae1fd7
v.kishoremani@gmail.com <AMMA1st*>
2025-06-21 12:40:58 +0530

    Create New Folder for C# and Android

---
+++ C#.Net/1DataType/1DataType/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("Int : "+c);
+
+            long a1 = 10;
+            long b1 = 20;
+            long c1 = a1 + b1;
+            Console.WriteLine("Long : " + c1);
+
+            float m = 3.14f;
+            float n = 2.34f;
+            float o = m + n;
+            Console.WriteLine("Float : " + o);
+
+            double p = 12.34323345;
+            double q = 19.343233457269;
+            double r = p + q;
+            Console.WriteLine("Double : " + r);
+            Console.ReadLine();
+
+        }
+    }
+}

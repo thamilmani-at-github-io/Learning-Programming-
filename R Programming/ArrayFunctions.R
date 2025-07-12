69e69af706f259c03aff9ff08863da9b72a69833
v.kishoremani@gmail.com <AMMA1st*>
2025-07-12 12:07:13 +0530

    syllabus

--- R Programming/ArrayFunctions.R
+++ R Programming/ArrayFunctions.R
@@ -11,7 +11,7 @@ v1 <- c(1,2,3,4,5,6,7,8,9)
 v2 <- c(11,12,13,14,15,16,17,18,19)
 v3 <- c(21,22,23,24,25,26,27,28,29)
 print(v1+v2)
-print(v1-v2)
+print(v2-v1)
 print(v1*v2)
 print(v1/v2)
 
@@ -23,3 +23,22 @@ row.names <- c("row 1","row 2","row 3")
 Matrix.names <- c("Matrix 1","Matrix 2","Matrix 3")
 result <- array(c(v1,v2,v3),dim = c(3,3,3),dimnames = list(row.names,column.names,Matrix.names))
 print(result)
+print(result[1,2,4])
+
+#Manipulating array
+V1<-c(5,9,3)
+V2<-c(10,11,12,13,14,15)
+array1<-array(c(V1,V2),dim = c(3,3))
+print(array1)
+
+#Calculations Across Array Element
+print(array1)
+result<-apply(array1,c(1),sum)
+print(result)
+
+print(array1)
+result<-apply(array1,c(2),sum)
+print(result)
+
+
+
